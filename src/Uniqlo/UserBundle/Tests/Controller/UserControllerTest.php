09db45747583853fc5dc8d556b5c91a48def054e
Gmajoulet <gmajoulet@gmail.com>
2014-11-23 12:37:18 +0100

    Generating the UserBundle and creating the UserController

---
+++ src/Uniqlo/UserBundle/Tests/Controller/UserControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Uniqlo\UserBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {}
+}
---
+++ src/Uniqlo/UserBundle/UniqloUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Uniqlo\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UniqloUserBundle extends Bundle
+{
+}
